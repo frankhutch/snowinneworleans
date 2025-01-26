d1eaf59c5759f770113d78c899174c3995c8e427
Mono Kai <frankhutch@gmail.com>
2025-01-26 10:38:09 -0600

    update head and add og image

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+/* notes: 
+  to update site: 
+  1) npm run build 
+  2) copy contents from build dir and paste at root (replace everything if prompted) 
+  3) git add/commit/push 
+  4) confirm in github repo under actions (workflow should show deployment step completed)
+*/
+
 import React from 'react';
 import Gallery from './components/Gallery';
 import './App.css';
