d4d47b34c58b4dd7f2b37a540276d7f2d3bd175e
Mono Kai <frankhutch@gmail.com>
2025-01-26 18:03:20 -0600

    this is a commit. w00tz.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,3 @@
-/* notes: 
-  to update site: 
-  1) npm run build 
-  2) copy contents from build dir and paste at root (replace everything if prompted) 
-  3) git add/commit/push 
-  4) confirm in github repo under actions (workflow should show deployment step completed)
-*/
-
 import React from 'react';
 import { FiShoppingCart } from "react-icons/fi";
 import Gallery from './components/Gallery';
